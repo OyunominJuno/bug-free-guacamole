b42c49197c4f44cdaa538e001537865534d66a00
oyunomin munkhkhurel <juno@oyunomins-MacBook-Pro.local>
2025-11-23 19:44:56 -0800

    initial set up done

---
+++ tests/unit/test_bug_free_guacamole_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from bug_free_guacamole.bug_free_guacamole_stack import BugFreeGuacamoleStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in bug_free_guacamole/bug_free_guacamole_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = BugFreeGuacamoleStack(app, "bug-free-guacamole")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
